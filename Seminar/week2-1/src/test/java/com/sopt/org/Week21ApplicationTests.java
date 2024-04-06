2108155ed18bcb4641092dca1f447e4777463e29
DongHoon Lee <dhl1924@naver.com>
2024-04-06 17:13:02 +0900

    init: week2-1 초기세팅

---
+++ Seminar/week2-1/src/test/java/com/sopt/org/Week21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
