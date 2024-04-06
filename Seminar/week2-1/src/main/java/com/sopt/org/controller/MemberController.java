8d1c0b14f86871a43e2f6dd6965c6330bf6c4110
DongHoon Lee <dhl1924@naver.com>
2024-04-06 17:45:28 +0900

    feat(MemberController): 멤버 정보를 넣는 Post 요청 구현

---
+++ Seminar/week2-1/src/main/java/com/sopt/org/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.sopt.org.controller;
+
+import com.sopt.org.service.MemberService;
+import com.sopt.org.service.dto.MemberCreateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity createMember(@RequestBody MemberCreateDto memberCreateDto) {
+        return ResponseEntity.created(URI.create(memberService.createMember(memberCreateDto))).build();
+    }
+}
