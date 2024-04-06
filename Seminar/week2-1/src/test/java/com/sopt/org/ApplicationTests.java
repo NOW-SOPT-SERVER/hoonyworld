2ff6561d667f44de842d7ea019e81fa371556603
DongHoon Lee <dhl1924@naver.com>
2024-04-06 17:42:42 +0900

    refactor(ApplicationTests): 이름 리팩토링

--- Seminar/week2-1/src/test/java/com/sopt/org/ApplicationTests.java
+++ Seminar/week2-1/src/test/java/com/sopt/org/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Week21ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
