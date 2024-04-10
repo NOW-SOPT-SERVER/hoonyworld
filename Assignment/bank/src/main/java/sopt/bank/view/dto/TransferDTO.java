327a69107697a592fcde241521124b2243cdf074
DongHoon Lee <dhl1924@naver.com>
2024-04-11 00:47:33 +0900

    refactor(TransferDTO): 계좌이체 DTO를 레코드로 수정

--- Assignment/bank/src/main/java/sopt/bank/view/dto/TransferDTO.java
+++ Assignment/bank/src/main/java/sopt/bank/view/dto/TransferDTO.java
@@ -1,25 +1,8 @@
 package sopt.bank.view.dto;
 
-public class TransferDTO {
-    private String sourceAccountNumber;
-    private String destinationAccountNumber;
-    private double amount;
-
-    public TransferDTO(String sourceAccountNumber, String destinationAccountNumber, double amount) {
-        this.sourceAccountNumber = sourceAccountNumber;
-        this.destinationAccountNumber = destinationAccountNumber;
-        this.amount = amount;
-    }
-
-    public String getSourceAccountNumber() {
-        return sourceAccountNumber;
-    }
-
-    public String getDestinationAccountNumber() {
-        return destinationAccountNumber;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
+public record TransferDTO(
+        String sourceAccountNumber,
+        String destinationAccountNumber,
+        double amount
+) {
 }
