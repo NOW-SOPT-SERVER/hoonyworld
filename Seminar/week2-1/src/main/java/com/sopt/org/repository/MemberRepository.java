11d4d9e25d7c59ddad9990e95ab869f2e286701c
DongHoon Lee <dhl1924@naver.com>
2024-04-10 17:24:32 +0900

    feat: 커밋 수정

---
+++ Seminar/week2-1/src/main/java/com/sopt/org/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.sopt.org.repository;
+
+import com.sopt.org.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
