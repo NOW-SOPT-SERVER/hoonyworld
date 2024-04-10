553b28a81c15ba12924b262996a21dfdbb77cd73
DongHoon Lee <dhl1924@naver.com>
2024-04-10 21:11:36 +0900

    feat(IllegalArgumentExceptionType): 예외 상수 추가

--- Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
+++ Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
@@ -4,7 +4,8 @@ public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumen
 
     INVALID_MENU("[ERROR] 유효하지 않은 메뉴입니다. 다시 입력해 주세요."),
     INVALID_ACCOUNT_NUMBER_LENGTH("[ERROR] 계좌번호의 숫자는 14자리 입니다."),
-    INVALID_ACCOUNT_NUMBER("[ERROR] 계좌번호는 숫자로 이루어져 있습니다.");
+    INVALID_ACCOUNT_NUMBER("[ERROR] 계좌번호는 숫자로 이루어져 있습니다."),
+    INVALID_AMOUNT("[ERROR] 금액은 양수여야 합니다. 다시 입력하세요.");
 
     private final String message;
 
