bdd92ef7d937a3b80685b6236c360eb91ab31593
DongHoon Lee <dhl1924@naver.com>
2024-04-11 00:47:11 +0900

    refactor(DepositDTO): 입금 DTO를 레코드로 수정

--- Assignment/bank/src/main/java/sopt/bank/view/dto/DepositDTO.java
+++ Assignment/bank/src/main/java/sopt/bank/view/dto/DepositDTO.java
@@ -1,20 +1,8 @@
 package sopt.bank.view.dto;
 
-public class DepositDTO {
-    private String accountNumber;
-    private double amount;
-
-    public DepositDTO(String accountNumber, double amount) {
-        this.accountNumber = accountNumber;
-        this.amount = amount;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
+public record DepositDTO(
+        String accountNumber,
+        double amount
+) {
 }
 
