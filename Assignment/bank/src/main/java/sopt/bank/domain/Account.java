01af7f1fc0c1d2b3f332c77eb8c9372621da2469
DongHoon Lee <dhl1924@naver.com>
2024-04-10 21:37:17 +0900

    feat(Account): 입금, 출금, 잔액 조회를 관리하는 도메인 추가

---
+++ Assignment/bank/src/main/java/sopt/bank/domain/Account.java
@@ -0,0 +1,31 @@
+package sopt.bank.domain;
+
+import sopt.bank.view.viewConstant.IllegalArgumentExceptionType;
+
+public class Account {
+    private String accountNumber;
+    private String owner;
+    private double balance;
+
+    public Account(String accountNumber, String owner, double balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw IllegalArgumentExceptionType.INSUFFICIENT_BALANCE.getException();
+        } // 추후 컨트롤러에서 try-catch로 예외처리
+
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
