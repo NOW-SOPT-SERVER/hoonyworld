6ee060183d7b0f8728136556465135ea51f6d20d
DongHoon Lee <dhl1924@naver.com>
2024-04-10 21:35:56 +0900

    feat(IllegalArgumentExceptionType): 잔액보다 많이 인출할 때 보여주는 예외메시지 추가

--- Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
+++ Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
@@ -7,7 +7,8 @@ public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumen
     INVALID_ACCOUNT_NUMBER_LENGTH("[ERROR] 계좌번호의 숫자는 14자리 입니다."),
     ONLY_ACCOUNT_NUMBER_DIGIT("[ERROR] 계좌번호는 숫자로 이루어져 있습니다."),
     INVALID_AMOUNT("[ERROR] 유효하지 않은 금액입니다."),
-    ONLY_AMOUNT_POSITIVE_NUMBER("[ERROR] 금액은 양수여야 합니다.");
+    ONLY_AMOUNT_POSITIVE_NUMBER("[ERROR] 금액은 양수여야 합니다."),
+    INSUFFICIENT_BALANCE("[ERROR] 출금액이 잔액보다 큽니다.");
 
     private final String message;
 
