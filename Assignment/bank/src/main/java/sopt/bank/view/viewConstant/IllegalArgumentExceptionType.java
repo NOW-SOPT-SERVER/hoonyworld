6ac719379dc597b25f9602ccd74415013f77e4c9
DongHoon Lee <dhl1924@naver.com>
2024-04-10 21:22:28 +0900

    refactor(IllegalArgumentExceptionType): 예외 상수 추가 및 메시지 변경

--- Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
+++ Assignment/bank/src/main/java/sopt/bank/view/viewConstant/IllegalArgumentExceptionType.java
@@ -2,10 +2,12 @@ package sopt.bank.view.viewConstant;
 
 public enum IllegalArgumentExceptionType implements ExceptionType<IllegalArgumentException> {
 
-    INVALID_MENU("[ERROR] 유효하지 않은 메뉴입니다. 다시 입력해 주세요."),
+    INVALID_MENU("[ERROR] 유효하지 않은 메뉴입니다."),
+    INVALID_ACCOUNT_NUMBER("[ERROR] 유효하지 않은 계좌번호입니다."),
     INVALID_ACCOUNT_NUMBER_LENGTH("[ERROR] 계좌번호의 숫자는 14자리 입니다."),
-    INVALID_ACCOUNT_NUMBER("[ERROR] 계좌번호는 숫자로 이루어져 있습니다."),
-    INVALID_AMOUNT("[ERROR] 금액은 양수여야 합니다. 다시 입력하세요.");
+    ONLY_ACCOUNT_NUMBER_DIGIT("[ERROR] 계좌번호는 숫자로 이루어져 있습니다."),
+    INVALID_AMOUNT("[ERROR] 유효하지 않은 금액입니다."),
+    ONLY_AMOUNT_POSITIVE_NUMBER("[ERROR] 금액은 양수여야 합니다.");
 
     private final String message;
 
