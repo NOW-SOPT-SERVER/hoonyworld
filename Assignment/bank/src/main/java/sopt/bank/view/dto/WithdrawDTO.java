36a5c65a5c8aea65aada5cc1b4b5920b9d506c1f
DongHoon Lee <dhl1924@naver.com>
2024-04-11 00:47:22 +0900

    refactor(WithdrawDTO): 출금 DTO를 레코드로 수정

--- Assignment/bank/src/main/java/sopt/bank/view/dto/WithdrawDTO.java
+++ Assignment/bank/src/main/java/sopt/bank/view/dto/WithdrawDTO.java
@@ -1,19 +1,7 @@
 package sopt.bank.view.dto;
 
-public class WithdrawDTO {
-    private String accountNumber;
-    private double amount;
-
-    public WithdrawDTO(String accountNumber, double amount) {
-        this.accountNumber = accountNumber;
-        this.amount = amount;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
+public record WithdrawDTO(
+        String accountNumber,
+        double amount
+) {
 }
