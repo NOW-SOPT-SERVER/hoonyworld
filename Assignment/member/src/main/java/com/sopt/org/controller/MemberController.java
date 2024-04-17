43b4168828a7f09c4fccb0582bb2c2a4e6b63b12
DongHoon Lee <dhl1924@naver.com>
2024-04-17 20:28:44 +0900

    feat(MemberController): MemberFindDto 객체 리스트를 반환하는 GET 요청 구현

--- Assignment/member/src/main/java/com/sopt/org/controller/MemberController.java
+++ Assignment/member/src/main/java/com/sopt/org/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,5 +34,10 @@ public class MemberController {
     ) {
         memberService.deleteMemberById(memberId);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberFindDto>> findAllMembers() {
+        return ResponseEntity.ok(memberService.findAllMembers());
     }
 }
