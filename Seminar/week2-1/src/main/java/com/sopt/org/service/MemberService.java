29ec7c5afb38f8a33d218952c4b482588aab246c
DongHoon Lee <dhl1924@naver.com>
2024-04-17 17:56:16 +0900

    feat(MemberService): 특정 회원 ID에 대한 정보를 조회하고 결과를 MemberFindDto 객체로 변환 후 반환하는 메서드 구현

--- Seminar/week2-1/src/main/java/com/sopt/org/service/MemberService.java
+++ Seminar/week2-1/src/main/java/com/sopt/org/service/MemberService.java
@@ -3,6 +3,8 @@ package com.sopt.org.service;
 import com.sopt.org.domain.Member;
 import com.sopt.org.repository.MemberRepository;
 import com.sopt.org.service.dto.MemberCreateDto;
+import com.sopt.org.service.dto.MemberFindDto;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,13 @@ public class MemberService {
     public String createMember(MemberCreateDto memberCreateDto) {
         Member member = memberRepository.save(Member.create(memberCreateDto.name(), memberCreateDto.part(), memberCreateDto.age()));
         return member.getId().toString();
+    }
+
+    public MemberFindDto findMemberById(
+            Long memberId
+    ) {
+        return MemberFindDto.of(memberRepository.findById(memberId).orElseThrow(
+                () -> new EntityNotFoundException("ID에 해당하는 사용자가 존재하지 않습니다.")
+        ));
     }
 }
