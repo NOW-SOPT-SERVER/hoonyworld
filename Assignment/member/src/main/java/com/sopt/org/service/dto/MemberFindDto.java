4fd1857e12742c7c89d226e84b3a804f174954e3
DongHoon Lee <dhl1924@naver.com>
2024-04-18 11:31:22 +0900

    refactor(MemberFindDto): List<Member>를 List<MemberFindDto>로 변환하는 로직을 새로운 정적 팩토리 메서드에 옮기기

--- Assignment/member/src/main/java/com/sopt/org/service/dto/MemberFindDto.java
+++ Assignment/member/src/main/java/com/sopt/org/service/dto/MemberFindDto.java
@@ -3,6 +3,8 @@ package com.sopt.org.service.dto;
 import com.sopt.org.domain.Member;
 import com.sopt.org.domain.Part;
 
+import java.util.List;
+
 public record MemberFindDto(
         String name,
         Part part,
@@ -13,5 +15,13 @@ public record MemberFindDto(
             Member member
     ) {
         return new MemberFindDto(member.getName(), member.getPart(), member.getAge());
+    }
+
+    public static List<MemberFindDto> listOf(
+            List<Member> members
+    ) {
+        return members.stream()
+                .map(MemberFindDto::of)
+                .toList();
     }
 }
