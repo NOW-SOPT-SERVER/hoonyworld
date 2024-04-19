76a30bcc2808af6ec4a6ae4a34098ff0f6c86edc
DongHoon Lee <dhl1924@naver.com>
2024-04-20 03:03:38 +0900

    init: initial commit

---
+++ Seminar/week3-1/src/test/java/com/sopt/org/week31/Week31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.org.week31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
