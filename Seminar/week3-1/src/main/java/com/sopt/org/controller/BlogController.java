7bf0ef829e630dbe8a11f2062f28c52252383576
DongHoon Lee <dhl1924@naver.com>
2024-05-01 20:51:10 +0900

    feat(BlogController): 블로그 생성 API를 제공하는 컨트롤러 생성

---
+++ Seminar/week3-1/src/main/java/com/sopt/org/controller/BlogController.java
@@ -0,0 +1,28 @@
+package com.sopt.org.controller;
+
+import com.sopt.org.common.dto.SuccessMessage;
+import com.sopt.org.common.dto.SuccessStatusResponse;
+import com.sopt.org.service.dto.BlogCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class BlogController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/blog")
+    public ResponseEntity<SuccessStatusResponse> createBlog(
+            @RequestHeader Long memberId,
+            @RequestBody BlogCreateRequest blogCreateRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).header(
+                        "Location",
+                        blogService.create(memberId, blogCreateRequest))
+                .body(SuccessStatusResponse.of(SuccessMessage.BLOG_CREATE_SUCCESS));
+    }
+}
+
