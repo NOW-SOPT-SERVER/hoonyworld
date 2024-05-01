34e6bd1c8d5d52db98765cb92483c4bf01a7259b
DongHoon Lee <dhl1924@naver.com>
2024-05-01 23:51:36 +0900

    refactor(SuccessMessage): 성공 메시지 추가

--- Assignment/blog/src/main/java/com/sopt/org/common/dto/SuccessMessage.java
+++ Assignment/blog/src/main/java/com/sopt/org/common/dto/SuccessMessage.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(),"블로그 생성이 완료되었습니다."),
-    ;
+    POST_CREATE_SUCCESS(HttpStatus.CREATED.value(),"게시글 생성이 완료되었습니다."),
+    POST_UPDATE_SUCCESS(HttpStatus.OK.value(),"게시글이 성공적으로 업데이트되었습니다."),
+    POST_RETRIEVED_SUCCESS(HttpStatus.OK.value(),"게시글 조회 성공");
     private final int status;
     private final String message;
 }
