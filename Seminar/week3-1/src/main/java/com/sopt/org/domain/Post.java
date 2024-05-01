a43fcc400b5f50b441206fcd2c99da592bc4ea54
DongHoon Lee <dhl1924@naver.com>
2024-05-01 19:48:03 +0900

    feat(Post): Post 엔티티 생성

---
+++ Seminar/week3-1/src/main/java/com/sopt/org/domain/Post.java
@@ -0,0 +1,22 @@
+package com.sopt.org.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Blog blog;
+}
