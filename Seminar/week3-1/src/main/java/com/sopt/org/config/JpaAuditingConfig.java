22b72b979989411a56d8a529d4ecb07b0bf6d699
DongHoon Lee <dhl1924@naver.com>
2024-05-01 21:46:33 +0900

    refactor(JpaAuditingConfig): 폴더 경로 이동

--- Seminar/week3-1/src/main/java/com/sopt/org/config/JpaAuditingConfig.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.sopt.org.common;
+package com.sopt.org.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
