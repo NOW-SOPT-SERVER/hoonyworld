50355831aa98970e317d0bb7a7a95ff71d0d766d
DongHoon Lee <dhl1924@naver.com>
2024-05-01 22:44:58 +0900

    refactor(ErrorResponse): of 메서드 오버로딩

--- Seminar/week3-1/src/main/java/com/sopt/org/common/dto/ErrorResponse.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/common/dto/ErrorResponse.java
@@ -6,6 +6,10 @@ public record ErrorResponse(
         int status,
         String message
 ) {
+    public static ErrorResponse of(int status, String message) {
+        return new ErrorResponse(status, message);
+    }
+
     public static ErrorResponse of(ErrorMessage errorMessage) {
         return new ErrorResponse(errorMessage.getStatus(), errorMessage.getMessage());
     }
