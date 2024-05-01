d21f0f0f33a249f00d0d8abbfbabf99e9685071e
DongHoon Lee <dhl1924@naver.com>
2024-05-01 21:59:43 +0900

    chore(ErrorResponse): import문 추가

--- Seminar/week3-1/src/main/java/com/sopt/org/common/dto/ErrorResponse.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/common/dto/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.sopt.org.common.dto;
 
+import com.sopt.org.exception.message.ErrorMessage;
+
 public record ErrorResponse(
         int status,
         String message
 ) {
-    public static ErrorResponse of(int status, String message) {
-        return new ErrorResponse(status, message);
+    public static ErrorResponse of(ErrorMessage errorMessage) {
+        return new ErrorResponse(errorMessage.getStatus(), errorMessage.getMessage());
     }
 }
