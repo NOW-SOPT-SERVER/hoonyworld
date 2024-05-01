327940d1f27cc1735c16e96a37fe5d6e79a36ffe
DongHoon Lee <dhl1924@naver.com>
2024-05-01 22:44:33 +0900

    refactor(BlogTitleUpdateRequest): Validation 추가(@Size)

--- Seminar/week3-1/src/main/java/com/sopt/org/service/dto/BlogTitleUpdateRequest.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/service/dto/BlogTitleUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.sopt.org.service.dto;
 
+import jakarta.validation.constraints.Size;
+
 public record BlogTitleUpdateRequest(
+        @Size(max = 5 , message = "블로그 제목이 최대 글자 수(5자)를 초과했습니다.")
         String title
 ) {
 }
