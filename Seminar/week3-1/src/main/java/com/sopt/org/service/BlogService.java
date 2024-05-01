7f0ae2468bea846dde372416adcf2db50cdd4e19
DongHoon Lee <dhl1924@naver.com>
2024-05-01 21:18:43 +0900

    refactor(BlogService): create 메서드 매개변수 리팩토링

--- Seminar/week3-1/src/main/java/com/sopt/org/service/BlogService.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/service/BlogService.java
@@ -2,6 +2,7 @@ package com.sopt.org.service;
 
 import com.sopt.org.domain.Blog;
 import com.sopt.org.domain.Member;
+import com.sopt.org.repository.BlogRepository;
 import com.sopt.org.service.dto.BlogCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,8 @@ public class BlogService {
     private final MemberService memberService;
 
     public String create(Long memberId, BlogCreateRequest blogCreateRequest) {
-        //member찾기
         Member member = memberService.findById(memberId);
-        Blog blog = blogRepository.save(Blog.create(member, blogCreateRequest.title(), createRequest.description()););
+        Blog blog = blogRepository.save(Blog.create(member, blogCreateRequest));
         return blog.getId().toString();
     }
 }
