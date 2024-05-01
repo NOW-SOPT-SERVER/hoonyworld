678b5dec2df25f179b83fd52c0662123cca9999e
DongHoon Lee <dhl1924@naver.com>
2024-05-01 19:47:06 +0900

    feat(Blog): Blog 엔티티 생성

---
+++ Seminar/week3-1/src/main/java/com/sopt/org/domain/Blog.java
@@ -0,0 +1,23 @@
+package com.sopt.org.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Column(length = 200)
+    private String title;
+
+    private String description;
+}
