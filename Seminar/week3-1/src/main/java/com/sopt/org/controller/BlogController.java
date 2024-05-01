1628f7c6a61852e7397aa38b7828f50fae0424ea
DongHoon Lee <dhl1924@naver.com>
2024-05-01 22:28:26 +0900

    feat(BlogController): title 업데이트 하는 PATCH API 메서드 추가

--- Seminar/week3-1/src/main/java/com/sopt/org/controller/BlogController.java
+++ Seminar/week3-1/src/main/java/com/sopt/org/controller/BlogController.java
@@ -4,6 +4,7 @@ import com.sopt.org.common.dto.SuccessMessage;
 import com.sopt.org.common.dto.SuccessStatusResponse;
 import com.sopt.org.service.BlogService;
 import com.sopt.org.service.dto.BlogCreateRequest;
+import com.sopt.org.service.dto.BlogTitleUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,15 @@ public class BlogController {
                         "Location",
                         blogService.create(memberId, blogCreateRequest))
                 .body(SuccessStatusResponse.of(SuccessMessage.BLOG_CREATE_SUCCESS));
+    }
+
+    @PatchMapping("/blog/{blogId}/title")
+    public ResponseEntity updateBlogTitle(
+            @PathVariable Long blogId,
+            @RequestBody BlogTitleUpdateRequest blogTitleUpdateRequest
+    ) {
+        blogService.updateTitle(blogId, blogTitleUpdateRequest);
+        return ResponseEntity.noContent().build();
     }
 }
 
