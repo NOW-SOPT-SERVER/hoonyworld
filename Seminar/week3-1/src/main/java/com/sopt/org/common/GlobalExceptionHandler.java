8021fc9db48955286d9da06f9eab2b249e71b946
DongHoon Lee <dhl1924@naver.com>
2024-05-01 21:41:39 +0900

    feat(GlobalExceptionHandler):  전역 예외 처리를 담당하는 클래스 생성
    - 예외가 발생했을 때, 예외를 잡아서 사용자에게 적절한 HTTP 응답을 반환

---
+++ Seminar/week3-1/src/main/java/com/sopt/org/common/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.sopt.org.common;
+
+import com.sopt.org.common.dto.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    }
+}
