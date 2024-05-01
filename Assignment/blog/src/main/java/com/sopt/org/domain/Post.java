f9f5cca694d8f8b5a13de927e315f7a486eb4551
DongHoon Lee <dhl1924@naver.com>
2024-05-01 23:07:23 +0900

    feat(Post): 정적 팩토리 메서드 추가

--- Assignment/blog/src/main/java/com/sopt/org/domain/Post.java
+++ Assignment/blog/src/main/java/com/sopt/org/domain/Post.java
@@ -1,6 +1,8 @@
 package com.sopt.org.domain;
 
+import com.sopt.org.service.dto.PostCreateRequest;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +21,19 @@ public class Post extends BaseTimeEntity{
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Blog blog;
+
+    public static Post create(final PostCreateRequest postCreateRequest, final Blog blog) {
+        return Post.builder()
+                .title(postCreateRequest.title())
+                .content(postCreateRequest.content())
+                .blog(blog)
+                .build();
+    }
+
+    @Builder
+    public Post(Blog blog, String title, String content) {
+        this.blog = blog;
+        this.title = title;
+        this.content = content;
+    }
 }
