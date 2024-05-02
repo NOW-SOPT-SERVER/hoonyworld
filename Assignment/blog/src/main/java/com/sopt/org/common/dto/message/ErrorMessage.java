a82f89487eecae839afd0b1e1741d41d1b6e072b
DongHoon Lee <dhl1924@naver.com>
2024-05-02 18:23:34 +0900

    feat(ErrorMessage): 블로그 소유자가 아닌데 글을 쓰는 경우의 예외메시지 추가

--- Assignment/blog/src/main/java/com/sopt/org/common/dto/message/ErrorMessage.java
+++ Assignment/blog/src/main/java/com/sopt/org/common/dto/message/ErrorMessage.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
     MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
     BLOG_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 블로그가 존재하지 않습니다"),
-    POST_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 포스트가 존재하지 않습니다");
+    POST_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 포스트가 존재하지 않습니다"),
+    NOT_OWNER_OF_THIS_BLOG(HttpStatus.FORBIDDEN.value(), "이 블로그의 소유자가 아닙니다.");
 
     private final int status;
     private final String message;
