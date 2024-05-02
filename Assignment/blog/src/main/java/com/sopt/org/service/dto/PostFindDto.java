48bcea318f11b332e212d96129137a4bc4144cb3
DongHoon Lee <dhl1924@naver.com>
2024-05-02 15:39:49 +0900

    refactor(PostFindDto): 블로그의 소유주도 반환하도록 수정

--- Assignment/blog/src/main/java/com/sopt/org/service/dto/PostFindDto.java
+++ Assignment/blog/src/main/java/com/sopt/org/service/dto/PostFindDto.java
@@ -1,14 +1,16 @@
 package com.sopt.org.service.dto;
 
 import com.sopt.org.domain.Blog;
+import com.sopt.org.domain.Member;
 import com.sopt.org.domain.Post;
 
 public record PostFindDto(
         String title,
         String content,
-        Blog blog
+        Blog blog,
+        Member member
 ) {
     public static PostFindDto from(Post post) {
-        return new PostFindDto(post.getTitle(), post.getContent(), post.getBlog());
+        return new PostFindDto(post.getTitle(), post.getContent(), post.getBlog(), post.getBlog().getMember());
     }
 }
