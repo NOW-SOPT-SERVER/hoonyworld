18abe94abd35236d3a3aaf6e68e2e6b950572970
DongHoon Lee <dhl1924@naver.com>
2024-05-02 10:57:33 +0900

    feat(SuccessMessage): 블로그 제목 업데이트 성공 메시지 추가

--- Assignment/blog/src/main/java/com/sopt/org/common/dto/message/SuccessMessage.java
+++ Assignment/blog/src/main/java/com/sopt/org/common/dto/message/SuccessMessage.java
@@ -10,7 +10,8 @@ public enum SuccessMessage {
 
     BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(),"블로그 생성이 완료되었습니다."),
     POST_CREATE_SUCCESS(HttpStatus.CREATED.value(),"게시글 생성이 완료되었습니다."),
-    POST_UPDATE_SUCCESS(HttpStatus.OK.value(),"게시글이 성공적으로 업데이트되었습니다."),
+    BLOG_TITLE_UPDATE_SUCCESS(HttpStatus.OK.value(),"블로그 제목이 성공적으로 업데이트되었습니다."),
+    POST_CONTENT_UPDATE_SUCCESS(HttpStatus.OK.value(),"게시글 내용이 성공적으로 업데이트되었습니다."),
     POST_RETRIEVED_SUCCESS(HttpStatus.OK.value(),"게시글 조회 성공");
     private final int status;
     private final String message;
