4feb8e125f098f0e8d168d35c9e924ebff1b9dec
DongHoon Lee <dhl1924@naver.com>
2024-05-03 12:01:19 +0900

    feat(GlobalExceptionHandler): 커스텀한 UnauthorizedBlogAccessException 추가

--- Assignment/blog/src/main/java/com/sopt/org/common/GlobalExceptionHandler.java
+++ Assignment/blog/src/main/java/com/sopt/org/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sopt.org.common;
 
 import com.sopt.org.common.dto.ErrorResponse;
 import com.sopt.org.exception.NotFoundException;
+import com.sopt.org.exception.UnauthorizedBlogAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,13 +14,24 @@ import java.util.Objects;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 커스텀한 NotFoundException
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e.getErrorMessage().getStatus(), e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.of(e.getErrorMessage().getStatus(), e.getMessage()));
     }
 
+    // @Valid 어노테이션 유효성 검사 -> 에러 시 MethodArgumentNotValidException
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
+    }
+
+    // 커스텀한 UnauthorizedBlogAccessException
+    @ExceptionHandler(UnauthorizedBlogAccessException.class)
+    protected ResponseEntity<ErrorResponse> handleUnauthorizedBlogAccessException(UnauthorizedBlogAccessException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ErrorResponse.of(e.getErrorMessage().getStatus(), e.getMessage()));
     }
 }
