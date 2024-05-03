dfadd0978d5a6b129648ed5cdd3b50b8e80def84
DongHoon Lee <dhl1924@naver.com>
2024-05-03 14:17:50 +0900

    refactor(BlogTitleUpdateRequestDto): 최대 글자 수 수정

--- Assignment/blog/src/main/java/com/sopt/org/service/dto/BlogTitleUpdateRequestDto.java
+++ Assignment/blog/src/main/java/com/sopt/org/service/dto/BlogTitleUpdateRequestDto.java
@@ -3,7 +3,7 @@ package com.sopt.org.service.dto;
 import jakarta.validation.constraints.Size;
 
 public record BlogTitleUpdateRequestDto(
-        @Size(max = 5 , message = "블로그 제목이 최대 글자 수(5자)를 초과했습니다.")
+        @Size(max = 15 , message = "블로그 제목이 최대 글자 수(15자)를 초과했습니다.")
         String title
 ) {
 }
