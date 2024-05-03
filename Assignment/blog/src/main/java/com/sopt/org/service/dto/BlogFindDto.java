284f93bf3d65884a99706b7ac2b7a71403e2155a
DongHoon Lee <dhl1924@naver.com>
2024-05-03 12:33:10 +0900

    fix(BlogFindDto): Jackson 직렬화 오류 수정
    - Hibernate 프록시 객체의 직렬화 문제 해결
    - BlogFindDto에서 Member 객체를 직접 참조할 때 발생하는 직렬화 오류를 수정하기 위해 DTO 구조 변경

--- Assignment/blog/src/main/java/com/sopt/org/service/dto/BlogFindDto.java
+++ Assignment/blog/src/main/java/com/sopt/org/service/dto/BlogFindDto.java
@@ -1,14 +1,16 @@
 package com.sopt.org.service.dto;
 
 import com.sopt.org.domain.Blog;
-import com.sopt.org.domain.Member;
 
 public record BlogFindDto(
-        Member member,
+        Long memberId,
         String title,
         String description
 ) {
     public static BlogFindDto from(Blog blog) {
-        return new BlogFindDto(blog.getMember(), blog.getTitle(), blog.getDescription());
+        return new BlogFindDto(
+                blog.getMember().getId(), // 프록시 초기화 없이 ID 접근
+                blog.getTitle(),
+                blog.getDescription());
     }
 }
