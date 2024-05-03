bcd2ca1da6da7503fdc0d507269c13431fa851c4
DongHoon Lee <dhl1924@naver.com>
2024-05-03 23:26:19 +0900

    refactor(BlogService): 동일한 로직 메서드로 분리

--- Assignment/blog/src/main/java/com/sopt/org/service/BlogService.java
+++ Assignment/blog/src/main/java/com/sopt/org/service/BlogService.java
@@ -28,25 +28,32 @@ public class BlogService {
 
     // GET 요청을 위한 DTO
     public BlogFindDto findBlogDtoById(Long blogId) {
-        Blog blog = blogRepository.findById(blogId).orElseThrow(
-                () -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND_BY_ID_EXCEPTION));
+        Blog blog = findBlogAndValidate(blogId);
         return BlogFindDto.from(blog);
     }
 
     public Blog findBlogById(Long blogId) {
-        Blog blog = blogRepository.findById(blogId).orElseThrow(
-                () -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND_BY_ID_EXCEPTION));
+        Blog blog = findBlogAndValidate(blogId);
         return blog;
     }
 
     @Transactional
     public void updateBlogTitle(Long memberId, Long blogId, BlogTitleUpdateRequestDto blogTitleUpdateRequestDto) {
-        Blog blog = blogRepository.findById(blogId).orElseThrow(
-                () -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND_BY_ID_EXCEPTION));
+        Blog blog = findBlogAndValidate(blogId);
+        validateBlogOwner(blog, memberId);
+        blog.setBlogTitle(blogTitleUpdateRequestDto.title());
+    }
 
+    // Blog 존재 확인 메서드
+    private Blog findBlogAndValidate(Long blogId) {
+        return blogRepository.findById(blogId).orElseThrow(
+                () -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND_BY_ID_EXCEPTION));
+    }
+
+    // Blog 소유권 검증 메서드
+    public void validateBlogOwner(Blog blog, Long memberId) {
         if (!blog.getMember().getId().equals(memberId)) {
             throw new UnauthorizedBlogAccessException(ErrorMessage.NOT_OWNER_OF_THIS_BLOG);
         }
-        blog.setBlogTitle(blogTitleUpdateRequestDto.title());
     }
 }
