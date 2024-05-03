3d692c4af546153ee2d738875f4bd4f46ead3e81
DongHoon Lee <dhl1924@naver.com>
2024-05-03 13:10:10 +0900

    feat(ErrorMessage): ErrorMessage 열거형에 새 메시지 추가

--- Assignment/blog/src/main/java/com/sopt/org/common/dto/message/ErrorMessage.java
+++ Assignment/blog/src/main/java/com/sopt/org/common/dto/message/ErrorMessage.java
@@ -10,7 +10,8 @@ public enum ErrorMessage {
     MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
     BLOG_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 블로그가 존재하지 않습니다"),
     POST_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 포스트가 존재하지 않습니다"),
-    NOT_OWNER_OF_THIS_BLOG(HttpStatus.FORBIDDEN.value(), "이 블로그의 소유자가 아닙니다.");
+    NOT_OWNER_OF_THIS_BLOG(HttpStatus.FORBIDDEN.value(), "해당 블로그의 소유자가 아닙니다."),
+    POST_DOES_NOT_BELONG_TO_BLOG(HttpStatus.FORBIDDEN.value(), "해당 포스트는 지정된 블로그에 속하지 않습니다.");
 
     private final int status;
     private final String message;
