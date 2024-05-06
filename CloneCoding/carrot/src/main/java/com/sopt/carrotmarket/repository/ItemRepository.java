d33d4dffbfc7915698426a3e50a03773da6a347a
DongHoon Lee <dhl1924@naver.com>
2024-05-06 18:33:16 +0900

    feat(ItemRepository): Item 클래스와 Member 클래스를 조인하여 지역별 아이템 리스트를 반환하는 메서드 구현

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/repository/ItemRepository.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/repository/ItemRepository.java
@@ -1,7 +1,13 @@
 package com.sopt.carrotmarket.repository;
 
 import com.sopt.carrotmarket.domain.Item;
+import com.sopt.carrotmarket.domain.constant.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    @Query("select i from Item i where i.member.location = :location")
+    List<Item> findByMemberLocation(Location location);
 }
