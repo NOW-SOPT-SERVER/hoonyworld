fc2f754bf900e84101069253ce8187f18bbd99ef
DongHoon Lee <dhl1924@naver.com>
2024-05-06 14:22:47 +0900

    feat(Like): Like 엔티티 생성

---
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/domain/Like.java
@@ -0,0 +1,22 @@
+package com.sopt.carrotmarket.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+}
