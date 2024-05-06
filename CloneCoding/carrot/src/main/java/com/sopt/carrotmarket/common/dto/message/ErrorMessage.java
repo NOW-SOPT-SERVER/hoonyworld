f63ac32895de592589f7cee2c4b794011a06847c
DongHoon Lee <dhl1924@naver.com>
2024-05-06 19:04:52 +0900

    feat(ErrorMessage): 아이템의 아이디가 존재하지 않을 때 보여주는 메시지 생성

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/ErrorMessage.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/ErrorMessage.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorMessage {
-    MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "[ERROR] ID에 해당하는 사용자가 존재하지 않습니다.");
+    MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "[ERROR] ID에 해당하는 사용자가 존재하지 않습니다."),
+    ITEM_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "[ERROR] ID에 해당하는 물건이 존재하지 않습니다.");
 
     private final int status;
     private final String message;
