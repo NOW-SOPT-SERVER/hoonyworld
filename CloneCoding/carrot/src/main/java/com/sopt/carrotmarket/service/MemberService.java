db511473495ca3e6e5fec7ccfac4aaf5a5cd9ebd
DongHoon Lee <dhl1924@naver.com>
2024-05-06 17:17:34 +0900

    feat(MemberService): MemberId로 DB에서 검색해 Member 객체의 정보를 반환하는 메서드 생성

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/service/MemberService.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.sopt.carrotmarket.service;
 
+import com.sopt.carrotmarket.common.dto.message.ErrorMessage;
 import com.sopt.carrotmarket.domain.Member;
+import com.sopt.carrotmarket.exception.NotFoundException;
 import com.sopt.carrotmarket.repository.MemberRepository;
 import com.sopt.carrotmarket.service.dto.MemberCreateRequest;
+import com.sopt.carrotmarket.service.dto.MemberFindResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,5 +19,20 @@ public class MemberService {
     public String createMember(MemberCreateRequest memberCreateRequest) {
         Member member = memberRepository.save(Member.create(memberCreateRequest.username(), memberCreateRequest.email(), memberCreateRequest.password(), memberCreateRequest.location()));
         return member.getId().toString();
+    }
+
+    public MemberFindResponse findMemberById(
+            Long memberId
+    ) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND_BY_ID_EXCEPTION)
+        );
+        return MemberFindResponse.from(member);
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND_BY_ID_EXCEPTION)
+        );
     }
 }
