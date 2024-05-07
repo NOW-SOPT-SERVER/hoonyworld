a9187b65691472964ac95c4c2418054f9ab1e4b9
DongHoon Lee <dhl1924@naver.com>
2024-05-07 17:50:41 +0900

    chore(SuccessMessage): 상수 오타 수정

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/SuccessMessage.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/SuccessMessage.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum SuccessMessage {
-    MEMBER_CREATE_SUCCESS(HttpStatus.CREATED.value(),"[SUCCESS] 블로그 생성이 완료되었습니다."),
+    MEMBER_CREATE_SUCCESS(HttpStatus.CREATED.value(),"[SUCCESS] 회원가입이 완료되었습니다."),
     ITEM_REGISTER_SUCCESS(HttpStatus.CREATED.value(), "[SUCCESS] 내 물건 등록이 완료되었습니다");
 
     private final int status;
