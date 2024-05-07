3f169a066fae8d9fcf9c6cf16f049367eca5985b
DongHoon Lee <dhl1924@naver.com>
2024-05-08 02:07:41 +0900

    feat(LikeService): return을 String으로 변환

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/service/LikeService.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/service/LikeService.java
@@ -4,6 +4,7 @@ import com.sopt.carrotmarket.domain.Item;
 import com.sopt.carrotmarket.domain.Like;
 import com.sopt.carrotmarket.domain.Member;
 import com.sopt.carrotmarket.repository.LikeRepository;
+import com.sopt.carrotmarket.service.dto.LikeCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +16,18 @@ public class LikeService {
     private final ItemService itemService;
     private final MemberService memberService;
 
-    public void addLike(Long memberId, Long blogId) {
-        Member member = memberService.findById(memberId);
-        Item item = itemService.findById(blogId);
+    public String addLike(LikeCreateRequest likeCreateRequest) {
+        Member member = memberService.findById(likeCreateRequest.memberId());
+        Item item = itemService.findById(likeCreateRequest.itemId());
 
         // 이미 좋아요한 경우 좋아요를 해제하고, 아닌 경우 좋아요를 추가.
         if (likeRepository.existsByMemberAndItem(member, item)) {
             likeRepository.deleteByMemberAndItem(member, item);
+            return null;
         } else {
             Like like = new Like(member, item);
             likeRepository.save(like);
+            return item.getId().toString();
         }
     }
 }
