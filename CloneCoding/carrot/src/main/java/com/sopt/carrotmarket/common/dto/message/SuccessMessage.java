b6556b730846a950473111c5c6fa696773a5777c
DongHoon Lee <dhl1924@naver.com>
2024-05-08 02:09:23 +0900

    feat(SuccessMessage): 좋아요 생성 및 취소 상수 추가

--- CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/SuccessMessage.java
+++ CloneCoding/carrot/src/main/java/com/sopt/carrotmarket/common/dto/message/SuccessMessage.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum SuccessMessage {
     MEMBER_CREATE_SUCCESS(HttpStatus.CREATED.value(),"[SUCCESS] 회원가입이 완료되었습니다."),
-    ITEM_REGISTER_SUCCESS(HttpStatus.CREATED.value(), "[SUCCESS] 내 물건 등록이 완료되었습니다");
+    ITEM_REGISTER_SUCCESS(HttpStatus.CREATED.value(), "[SUCCESS] 내 물건 등록이 완료되었습니다."),
+    EXTRA_LIKE_BUTTON(HttpStatus.CREATED.value(), "[SUCCESS] 좋아요 버튼을 누르셨습니다."),
+    DELETE_LIKE_BUTTON(HttpStatus.CREATED.value(), "[SUCCESS] 좋아요 버튼을 취소하셨습니다.");
 
     private final int status;
     private final String message;
