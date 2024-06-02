36c52d7e0bed726cc2d41b4aa3cd4c2b35355e4c
DongHoon Lee <dhl1924@naver.com>
2024-06-02 22:48:46 +0900

    fix(AuthenticationController): SON 객체를 사용해 리프레시 토큰을 전달하도록 변경

--- Assignment/security/src/main/java/com/sopt/org/auth/controller/AuthenticationController.java
+++ Assignment/security/src/main/java/com/sopt/org/auth/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.sopt.org.auth.redis.service.AuthenticationService;
 import com.sopt.org.auth.redis.service.RefreshTokenService;
 import com.sopt.org.auth.redis.service.dto.LoginRequest;
 import com.sopt.org.auth.redis.service.dto.LoginResponse;
+import com.sopt.org.auth.redis.service.dto.TokenRefreshRequest;
+import com.sopt.org.auth.redis.service.dto.TokenRefreshResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +24,13 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = authenticationService.login(loginRequest);
-        return ResponseEntity.ok(loginResponse);
+        return ResponseEntity.ok(LoginResponse.of(loginResponse.accessToken(), loginResponse.refreshToken()));
     }
 
     @PostMapping("/refresh-token")
-    public ResponseEntity<LoginResponse> refreshToken(@RequestBody String refreshToken) {
-        String newAccessToken = refreshTokenService.reissueAccessToken(refreshToken);
-        return ResponseEntity.ok(new LoginResponse(newAccessToken, refreshToken));
+    public ResponseEntity<TokenRefreshResponse> refreshToken(@RequestBody TokenRefreshRequest tokenRefreshRequest) {
+        String newRefreshToken = tokenRefreshRequest.newRefreshToken();
+        String newAccessToken = refreshTokenService.reissueAccessToken(newRefreshToken);
+        return ResponseEntity.ok(TokenRefreshResponse.of(newAccessToken, newRefreshToken));
     }
 }
