b97f756097732eda8e3d472eb426171cd005e71e
DongHoon Lee <dhl1924@naver.com>
2024-06-02 21:56:45 +0900

    feat(MemberRepository): 이름으로 멤버 객체를 식별 후 반환하는 메서드 구현

--- Assignment/security/src/main/java/com/sopt/org/repository/MemberRepository.java
+++ Assignment/security/src/main/java/com/sopt/org/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import com.sopt.org.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByName(String name);
 }
