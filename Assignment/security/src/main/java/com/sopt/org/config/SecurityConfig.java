ce963f4c834e7a1fc82fa27c646fd98fa323c24e
DongHoon Lee <dhl1924@naver.com>
2024-06-03 00:49:04 +0900

    chore(SecurityConfig): 불필요한 import문 삭제

--- Assignment/security/src/main/java/com/sopt/org/config/SecurityConfig.java
+++ Assignment/security/src/main/java/com/sopt/org/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import com.sopt.org.auth.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
